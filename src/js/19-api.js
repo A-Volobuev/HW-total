96f480850256a64d056f49a1cc2e6fc052c91548
A-Volobuev <Volobuev22.02@gmail.com>
2024-07-31 01:35:20 +0300

    another commit

---
+++ src/js/19-api.js
@@ -0,0 +1,97 @@
+// Импортирую настройки select из хбс
+import '../scss/19-api.scss'
+import CatApiService from './components/19-cat-api';
+import createOptionsInSelect from '../templates/19-select-option.hbs';
+import informationBlockMarkup from '../templates/19-informative-block.hbs';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+
+// Привязываем элементы 
+const selectBreed = document.querySelector('.breed-select');
+const informativeBlock = document.querySelector('.cat-info');
+const decorativeOption = document.querySelector('.decorative-option');
+const loader = document.querySelector('.container-loader');
+
+// Добавляем сюда наш класс запросов
+const catApiService = new CatApiService();
+
+// Слушатель событий на селектор
+selectBreed.addEventListener('change', onSelectBreedChange);
+    //Добавил что бы при первом клике убрать декоративный пустой option с select 
+selectBreed.addEventListener('click', function () {
+    decorativeOption.classList.add('is-hiden');
+});
+
+//------ Функция при выборе на селекте породы 
+function onSelectBreedChange(e) {
+    // Сохраняем выбранную в переменную, для дальнейшей загрузки полной информации
+    catApiService.breedId = e.currentTarget.value;
+
+    loader.classList.remove('is-hiden');
+    
+    // Добавляем разметку на конкретную породу
+    catApiService.fetchCatByBreed()
+    .then(data => {
+        // Очищаем страницу при выборе новой породы
+        clearInformationBlock();
+
+        createInformationBlockMarkup(data);
+
+        loader.classList.add('is-hiden')
+    })
+    .catch(error => {
+        Report.failure(
+            'Ошибка',
+            `${error}`,
+            'Okay',
+        );
+    });
+}
+
+// 3. создаем функцию запроса и добавляем выбор в селект
+function selectOption() {
+    selectBreed.classList.add('is-hiden')
+
+
+    catApiService.fetchBreeds()
+        .then(addSelectBreed)
+        .catch(error => {
+            Report.failure(
+                'Ошибка',
+                `${error}`,
+                'Okay',
+                );})
+
+    loader.classList.add('is-hiden')
+    selectBreed.classList.remove('is-hiden')
+}
+// 3. Вызываем фукнцию
+selectOption();
+
+// 3. Функция добавления option в select
+function addSelectBreed (data) {
+    selectBreed.insertAdjacentHTML('beforeend', createOptionsInSelect(data))
+}
+
+// 7. Функция добавляет разметку на страницу
+function createInformationBlockMarkup (data) {
+    informativeBlock.insertAdjacentHTML('beforeend',informationBlockMarkup(data)) 
+}
+
+// 7. Функция очистки разметки 
+function clearInformationBlock () {
+    informativeBlock.innerHTML = '';
+}
+
+
+// 1. Получаем ключ
+// 2. Делаем Гет запрос
+// 3. При успешном запросе, необходимо наполнить select.breed-select опциями так, чтобы value опции содержал id породы,
+    //  а в интерфейсе пользователю отображалось название породы. 
+// 4.Когда пользователь выбирает опцию в селекте, необходимо выполнять запрос за полной информацией о коте на ресурс
+    //  https://api.thecatapi.com/v1/images/search. Не забудь указать в этом запросе параметр строки запроса breed_ids 
+    // с идентификатором породы.
+// 5. Сохраняем выбраную породу в breedId
+// 6. fetchCatByBreed(breedId) которая ожидает идентификатор породы, делает HTTP-запрос и возвращает промис с данными 
+    // о коте - результатом запроса.
+// 7. Если запрос был успешный, под селектом, в блоке div.cat-info появляется изображение и развернутая информация о 
+    // коте: название породы, описание и темперамент.
---
+++ src/js/components/19-cat-api.js
@@ -0,0 +1,73 @@
+const API_KEY = 'live_7ad8upyX21GZXAiarpBVFKI96kSct8KgO5N7x1ungdbJekziVwWJNouLsXsd0P7L';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const options = {
+    headers: {
+        'X-Api-Key': API_KEY,
+    },
+};
+
+export default class CatApiService {
+    constructor() {
+        // 5. Сохраняем выбраную породу
+        this.breedId = '';
+    }
+
+    // Запрос для селекта
+    fetchBreeds() {
+        // настройки для запроса с сервера
+        const breedsUrl = `${BASE_URL}/breeds`
+
+        // Запрос
+        return fetch(breedsUrl, options)
+            .then(resolt => {
+                // console.log(resolt.ok)
+                // Нашел только такой способ (создать самому ошибку), потому что иначе поймать ее через catch в 
+                // основном джс файле я не знаю как (Срабатывает здесь на 21 строке и 30й)
+                if(!resolt.ok){
+                    throw new Error("Ошибка!");
+                }
+                return resolt.json()})
+            .then(data => {
+            console.log(data); 
+
+            return data;
+            });
+    }
+
+    // Запрос на информацию от селекта
+    fetchCatByBreed () {
+        // настройки для запроса с сервера
+        const selectBreedsUrl = `${BASE_URL}/images/search?breed_ids=${this.breedId}`
+
+        // Запрос
+        return fetch(selectBreedsUrl, options)
+        .then(resolt => {
+            if(!resolt.ok){
+                throw new Error("Ошибка!");
+            }
+            return resolt.json()})
+        .then(data => {
+            console.log(data);
+            return data;
+        })
+    }
+
+    get idBreed() {
+        return this.breedId;
+    }
+
+    set idBreed(newBreedId) {
+        this.breedId = newBreedId;
+    }
+}
+
+// export default // fetchBreeds - получаем породы кошек
+// fetch(`${BASE_URL}/breeds`)
+//     .then(resolt => resolt.json())
+//     .then(data => {
+//         console.log(data);
+
+        // Добавляю здесь, потому что эта функция выполняется позже и данные с фетча не получить.
+        // Добавляем в селкт выбор породы кошек
+        // selectBreed.insertAdjacentHTML('beforeend', selectOptions(data))
+//     });
